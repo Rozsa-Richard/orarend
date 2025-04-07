e42c5a7ebcf3018bac36bfe2ca34e7d8e64a7bc0
Rikky <rozsarichardzon@gmail.com>
2025-04-07 11:35:46 +0200

    folytatni

---
+++ orarend/Util/database.js
@@ -0,0 +1,46 @@
+import sqlite from 'sqlite3'
+const db = new sqlite.Database('./data/database.sqlite')
+
+export function dbAll(sql, params = []){
+    return new Promise((resolve,reject) => {
+        db.all(sql, params, (err, rows) => {
+            if (err) reject(err);
+            else resolve(rows);
+        });
+    });
+}
+
+export function dbGet(sql, params=[]){
+    return new Promise((resolve, reject) => {
+        db.get(sql, params, (err, row) =>{
+            if (err) reject(err);
+            else resolve(row);
+        });
+    });
+}
+
+export function dbRun(sql, params = []){
+    return new Promise((resolve, reject) => {
+        db.run(sql, params, function(err){
+            if (err) reject(err);
+            else resolve(this);
+        });
+    });
+}
+
+export async function initializeDatabase(){
+    await dbRun("DROP TABLE IF EXISTS orarend");
+    await dbRun("CREARE TABLE IF NOT EXISTS orarend (id INTIGER PRIMARY KEY AUTOINCREMENT, day STRING, classNumber INTEGER, className STRING)");
+
+    const classes = [
+        {day: "Hetfő", classNumber: 1, className: "Matek"},
+        {day: "Hetfő", classNumber: 2, className: "Angol"},
+        {day: "Hetfő", classNumber: 3, className: "Tesi"},
+        {day: "Hetfő", classNumber: 4, className: "Tesi"},
+        {day: "Hetfő", classNumber: 5, className: "Nyelvtan"},
+
+    ]
+    for (const i of classes){
+        await dbRun("INSERT INTO orarend (day,classNumber,className) VALUES (?, ?, ?);",[i.day, i.classNumber, i.className])
+    }
+}
---
+++ orarend/app.js
@@ -0,0 +1,72 @@
+import express from 'express'
+import { dbAll, initializeDatabase, dbGet,dbRun } from './Util/database.js'
+
+const app = express()
+app.use(express.json())
+
+app.get('/timetable', async (req, res)=> {
+    const timetable = await dbAll("SELECT * FROM orarend");
+    res.status(200).json(timetable)
+})
+
+app.get('/timetable/:id', async (req, res) => {
+    const id = req.params.id;
+    const thatclass = await dbGet("SELECT * FROM orarend WHERE id=?;", [id])
+    if (!thatclass) {
+        return res.status(400).json({message: "Class not found"})
+    }
+    else {
+        res.status(200).json(thatclass)
+    }
+})
+app.get('/')
+
+app.use((req, res, next, err) =>{
+    if (err){
+        res.status(500).json({message: 'Error ${err.message}'})
+    }
+})
+app.post("/timetable", async (req, res) => {
+    const {day, classNumber, className} = req.body;
+    if (!day || !classNumber || !className){
+        return res.status(400).json({message: "Missing some data"})
+    }
+    
+    const result = await dbRun("INSERT INTO orarend (day,classNumber,className) VALUES (?, ?, ?);",[day, classNumber,className])
+    res.status(201).json({id: result.lastID, day, classNumber, className});
+})
+
+app.put("/timetable/:id", async (req, res) => {
+    const id = req.params.id;
+    const thatclass = dbGet("SELECT * FROM orarend WHERE id = ?;",[id]);
+    if (!thatclass){
+        return res.status(404).json({message:"Class not found"});
+    }
+    const {day, classNumber, className} = req.body;
+    if (!day || !classNumber || !className){
+        return res.status(400).json({message:"Missing data"});
+    }
+    
+    await dbRun("UPDATE orarend SET day = ?, classNumber = ?, className = ? WHERE id = ?;",[day, classNumber, className,id]);
+    res.status(200).json({id,day,classNumber,className});
+})
+
+app.delete("/timetable/:id", async (req, res) => {
+    const id = req.params.id;
+    const thatclass = dbGet("SELECT * FROM orarend WHERE id = ?;",[id]);
+    if (!thatclass){
+        return res.status(404).json({message:"Missing data"});
+    }
+    await dbRun("DELETE FROM orarends WHERE id = ?;",[id]);
+    es.status(200).json({message:"delete sucsessful"});
+})
+
+
+async function startServer() {
+    await initializeDatabase();
+    app.listen(3000, ()=> {
+        console.log('Server runs on port 3000')
+    })
+}
+
+startServer();
